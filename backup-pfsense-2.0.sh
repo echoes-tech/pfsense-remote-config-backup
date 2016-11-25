5e67f571ca41ef14ab07d4515d6c7ab547c6d4f6
Florent Poinsaut <floSoX@users.noreply.github.com>
2016-11-25 23:18:45 +0100

    Add WGET variable

--- backup-pfsense-2.0.sh
+++ backup-pfsense-2.0.sh
@@ -10,6 +10,8 @@ BACKUP_URL="${URL}/diag_backup.php"
 DIR="/var/backups/pfsense"
 COOKIE="/tmp/cookies-${HOST}.txt"
 
+WGET='/usr/bin/wget -q --no-check-certificate'
+
 check_result() {
   if [ ${?} -ne 0 ]; then
     echo "ERROR: $1"
@@ -18,20 +20,20 @@ check_result() {
 }
 
 pre (){
-  magic="$(/usr/bin/wget -q --no-check-certificate "${URL}" -O - | grep "input type='hidden' name='__csrf_magic'" | sed -e 's/.*value="\(.*\)".*/\1/')"
+  csrf_magic="$($WGET -O- "${URL}" | grep "input type='hidden' name='__csrf_magic'" | sed -e 's/.*value="\(.*\)".*/\1/')"
 
-  /usr/bin/wget -q --no-check-certificate --keep-session-cookies --save-cookies "${COOKIE}" --post-data "login=Login&usernamefld=${USER}&passwordfld=${PASSWORD}&__csrf_magic=${magic}" ${BACKUP_URL} -O /dev/null
+  $WGET --keep-session-cookies --save-cookies "${COOKIE}" --post-data "login=Login&usernamefld=${USER}&passwordfld=${PASSWORD}&__csrf_magic=${csrf_magic}" ${BACKUP_URL} -O /dev/null
   check_result "ERROR: Could not login to '${HOST}'!"
 
   # Config seule. Pour pouvoir tracer les modification de config
-  /usr/bin/wget -q --no-check-certificate --keep-session-cookies --load-cookies "${COOKIE}" --post-data 'Submit=download&donotbackuprrd=on' ${BACKUP_URL} -O ${DIR}/config-${HOST}.xml
+  $WGET --keep-session-cookies --load-cookies "${COOKIE}" --post-data 'Submit=download&donotbackuprrd=on' ${BACKUP_URL} -O ${DIR}/config-${HOST}.xml
   check_result "Could not download configuration data from '${HOST}'!"
 
   # La meme chose, avec les données RRD en +
-  /usr/bin/wget -q --no-check-certificate --keep-session-cookies --load-cookies "${COOKIE}" --post-data 'Submit=download' ${BACKUP_URL} -O ${DIR}/config-${HOST}+rrd.xml
+  $WGET --keep-session-cookies --load-cookies "${COOKIE}" --post-data 'Submit=download' ${BACKUP_URL} -O ${DIR}/config-${HOST}+rrd.xml
   check_result "Could not download configuration + rrd data from '${HOST}'!"
 
-  /usr/bin/wget -q --no-check-certificate --load-cookies "${COOKIE}" "${URL}/index.php?logout" -O /dev/null
+  $WGET --load-cookies "${COOKIE}" "${URL}/index.php?logout" -O /dev/null
   check_result "ERROR: Could not logout of '${HOST}'!"
 
   /bin/rm -f /tmp/cookies-${HOST}.txt
